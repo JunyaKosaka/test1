31ecd1e60bd43ba63114209d5715e00161943f3a
JunyaKosaka <junya_in_farm@yahoo.co.jp>
2018-10-26 21:14:21 +0900

    Initial Commit

---
+++ AMNowClockView/AMNowClockView.h
@@ -0,0 +1,19 @@
+//
+//  AMNowClockView.h
+//  AMNowClockView
+//
+//  Created by Junya Kosaka on 2018/10/26.
+//  Copyright © 2018 Junya Kosaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMNowClockView.
+FOUNDATION_EXPORT double AMNowClockViewVersionNumber;
+
+//! Project version string for AMNowClockView.
+FOUNDATION_EXPORT const unsigned char AMNowClockViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMNowClockView/PublicHeader.h>
+
+
